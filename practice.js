eef4e315235a9c2e4dd586d4a62eedfaafdef2c1
Maddy Randle <maddyrandle318@gmail.com>
2023-06-09 21:54:28 -0500

    Check if user is under the age of 2 and print a new message if they are

--- practice.js
+++ practice.js
@@ -2,6 +2,8 @@ var age = 29;
 
 if (age >= 21) {
     console.log("Welcome to the website")
+} else if (age < 2) {
+    console.log("How are you even on this device?")
 } else {
     console.log("Come back when you're 21!")
 }
