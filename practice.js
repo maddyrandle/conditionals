be98de48cd5a50336aa6acc3d2712282675f28ad
Maddy Randle <maddyrandle318@gmail.com>
2023-06-09 22:07:51 -0500

    Create a username variable and print a unique message based on the value of the username

--- practice.js
+++ practice.js
@@ -1,4 +1,5 @@
 var age = 29;
+var username = "Maddy";
 
 if (age >= 21) {
     console.log("Welcome to the website")
@@ -6,4 +7,10 @@ if (age >= 21) {
     console.log("How are you even on this device?")
 } else {
     console.log("Come back when you're 21!")
+}
+
+if (username === "Oscar") {
+    console.log("Hello, Oscar!")
+} else {
+    console.log(`Nice to meet you, ${username}!`)
 }
